78149cebcb3a5114cc41368dde644c06b2586c99
imthur <arthur.aguiar8708@gmail.com>
2025-04-10 09:11:02 -0300

    Adicionando funções de adicionar, remover e pesquisar tarefas

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const TodoForm = ({ addTodo }) => {
   const [value, setValue] = useState("");
@@ -8,29 +8,32 @@ const TodoForm = ({ addTodo }) => {
     e.preventDefault();
     if (!value || !category) return;
     addTodo(value, category);
+
     setValue("");
+
     setCategory("");
   };
   return (
     <div className="todo-form">
-        <h2>Criar tarefa</h2>
-        <form onSubmit={handleSubmit}>
-            <input 
-            type="text" 
-            placeholder="Digite o título" 
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-            />
-              <select value={category} onChange={(e) => setCategory(e.target.value)}>
-                <option value="">Selecione uma categoria</option>
-                <option value="Trabalho">Trabalho</option>
-                <option value="Pessoal">Pessoal</option>
-                <option value="Estudos">Estudos</option>
-              </select>
-            <button type="submit">Criar Tarefa</button>
-        </form>
-    </div>
-  )
-}
+      <h2>Criar tarefa</h2>
 
-export default TodoForm
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Digite o título"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <select value={category} onChange={(e) => setCategory(e.target.value)}>
+          <option value="">Selecione uma categoria</option>
+          <option value="Trabalho">Trabalho</option>
+          <option value="Pessoal">Pessoal</option>
+          <option value="Estudos">Estudos</option>
+        </select>
+        <button type="submit">Criar Tarefa</button>
+      </form>
+    </div>
+  );
+};
+
+export default TodoForm;
